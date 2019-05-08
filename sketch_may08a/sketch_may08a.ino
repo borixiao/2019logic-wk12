fe88e46b16c84d765048a3d5affaba0e8c3d9027
borixiao <48972641+borixiao@users.noreply.github.com>
2019-05-08 11:48:02 +0800

    Add files via upload

---
+++ sketch_may08a/sketch_may08a.ino
@@ -0,0 +1,19 @@
+const int buttonPin=2;
+const int ledPin=13;
+int buttonState=0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin,OUTPUT);
+pinMode(buttonPin,INPUT);
+digitalWrite(buttonPin,HIGH);
+}
+
+void loop() {
+  buttonState=digitalRead(buttonPin);// put your main code here, to run repeatedly:
+if(buttonState== HIGH){
+  digitalWrite(ledPin,HIGH);
+}
+else{
+  digitalWrite(ledPin,LOW);
+}
+}
